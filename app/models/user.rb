199b1dbd9f3a519a6159003dffc887eb0a5d7ab7
soccerjesse <jishperu10@gmail.com>
2022-04-06 13:21:35 +0900

    Finish ch13.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
@@ -61,6 +62,9 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    Micropost.where("user_id = ?", self.id)
+  end
 
 
   private
